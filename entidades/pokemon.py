a5e135c86a12d2f856ea62ef68bdf1aa9d7faa91
silva <silva.rodrigo31@outlook.com>
2023-01-31 21:01:18 -0300

    Adicão de método para recuperar os dados de um pokemon

--- entidades/pokemon.py
+++ entidades/pokemon.py
@@ -1,72 +1,75 @@
 from typing import List, Dict
 import requests
-from cortipopokemon import Cor
+from entidades.cortipopokemon import Cor
 
 
-class Pokemon:
-    class Pokemom:
-        def __init__(self, resultado: Dict):
-            self._id = resultado['id']
-            self._name = resultado['name']
-            self._peso = resultado['weight']
-            self._tipos = [tipos['type']['name'] for tipos in resultado.get('types')]
-            self._cor = ''.join([cor.value for cor in Cor if self._tipos[0] in cor.name])
-            self._habilidade = [habilidades['ability']['name'] for habilidades in resultado['abilities']]
-            self._img = resultado['sprites']['other']['home']['front_default'] \
-                if resultado['sprites']['other']['home']['front_default'] is not None else \
-                resultado['sprites']['other']['official-artwork']['front_default']
-            self._estatisicas = {resultado['stats'][i]['stat']['name']:
-                                     resultado['stats'][i]['base_stat'] for i in
-                                 range(len(resultado['stats']))}
+class Pokemom:
+    def __init__(self, dados: Dict):
+        """
+        atributos para desserializar o json de pokemons
+        :param dados: dados dos pokemons em dicionários
+        """
+        self._id = dados['id']
+        self._name = dados['name']
+        self._peso = dados['weight']
+        self._tipos = [tipos['type']['name'] for tipos in dados.get('types')]
+        self._cor = ''.join([cor.value for cor in Cor if self._tipos[0] in cor.name])
+        self._habilidade = [habilidades['ability']['name'] for habilidades in dados['abilities']]
+        self._img = dados['sprites']['other']['home']['front_default'] \
+            if dados['sprites']['other']['home']['front_default'] is not None else \
+            dados['sprites']['other']['official-artwork']['front_default']
+        self._estatisicas = {dados['stats'][i]['stat']['name']:
+                                 dados['stats'][i]['base_stat'] for i in
+                             range(len(dados['stats']))}
 
-            self._moves = [resultado['moves'][i]['move']['name'] for i in range(len(resultado['moves']))]
-            self._locations = self.__get_location(resultado['location_area_encounters'])
+        self._moves = [dados['moves'][i]['move']['name'] for i in range(len(dados['moves']))]
+        self._locations = self.__get_location(dados['location_area_encounters'])
 
-        def __get_location(self, url: str) -> List[str]:
-            req = requests.get(url)
+    def __get_location(self, url: str) -> List[str]:
+        req = requests.get(url)
 
-            if req.status_code == 404:
-                location = []
-            else:
-                location = req.json()
-            return [location[i]['location_area']['name'] for i in range(len(location))]
+        if req.status_code == 404:
+            location = []
+        else:
+            location = req.json()
+        return [location[i]['location_area']['name'] for i in range(len(location))]
 
-        @property
-        def locations(self):
-            return self._locations
+    @property
+    def locations(self):
+        return self._locations
 
-        @property
-        def moves(self) -> List[str]:
-            return self._moves
+    @property
+    def moves(self) -> List[str]:
+        return self._moves
 
-        @property
-        def name(self) -> str:
-            return self._name
+    @property
+    def name(self) -> str:
+        return self._name
 
-        @property
-        def id(self) -> int:
-            return self._id
+    @property
+    def id(self) -> int:
+        return self._id
 
-        @property
-        def tipos(self) -> List[str]:
-            return self._tipos
+    @property
+    def tipos(self) -> List[str]:
+        return self._tipos
 
-        @property
-        def img(self) -> str:
-            return self._img
+    @property
+    def img(self) -> str:
+        return self._img
 
-        @property
-        def cor(self) -> str:
-            return self._cor
+    @property
+    def cor(self) -> str:
+        return self._cor
 
-        @property
-        def habilidade(self) -> List[str]:
-            return self._habilidade
+    @property
+    def habilidade(self) -> List[str]:
+        return self._habilidade
 
-        @property
-        def estatisticas(self) -> Dict[str, int]:
-            return self._estatisicas
+    @property
+    def estatisticas(self) -> Dict[str, int]:
+        return self._estatisicas
 
-        @property
-        def peso(self) -> str:
-            return self._peso
+    @property
+    def peso(self) -> str:
+        return self._peso
--- services/pokeapi.py
+++ services/pokeapi.py
@@ -1,8 +1,19 @@
+import json
+from typing import Dict
+
+import aiohttp
+
+
 class PokeAPI:
     def __init__(self):
         self.url = 'https://pokeapi.co/api/v2/'
 
-    async def get_pokemon(self, indice: int):
+    async def get_pokemon(self, indice: int) -> Dict:
+        """
+        Método para retornar um dicionario de pokenon
+        :param indice: incide do pokemon
+        :return: json
+        """
         async with aiohttp.ClientSession() as session:
             async with session.get(f'{self.url}pokemon/{indice}/') as response:
                 if response.status != 200:
