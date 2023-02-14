2c621699f3c90449527b5dca1d3acd8a9dbf6115
silva <silva.rodrigo31@outlook.com>
2023-02-13 21:57:40 -0300

    Seleção por tipo

--- componentes/telas.py
+++ componentes/telas.py
@@ -10,119 +10,234 @@ from services.pokeservice import main
 
 
 def gerar_cartoes(pokemons: List[Pokemom], tipo=None):
-    return dbc.Row(
-        dbc.Col(
-            [
-                dbc.Card(
-                    dbc.CardBody(
-                        [
-                            dbc.CardImg(src=pokemon.img,
-                                        id=f'{pokemon.name}',
-                                        className='class_img_pokemon'),
-                            html.P(f'{pokemon.id} - {pokemon.name.title()}',
-                                   id=f'id_nome_pokemon_{pokemon.id}',
-                                   className='class_nome_pokemon'),
-                            html.P(f' - '.join(pokemon.tipos).title(),
-                                   className='class_nome_pokemon'),
-                            dbc.Tabs(
-                                [
-                                    dbc.Tab(
-                                        [
-                                            dbc.Row(
-                                                [
-                                                    dbc.Col(
-                                                        html.Div(
-                                                            f'{chave.upper()} - {valor}',
-                                                            id=f'id_stats_name_{chave}',
-                                                            className='class_nome_pokemon'
+    if tipo in [cor.name for cor in Cor]:
+        return dbc.Row(
+            dbc.Col(
+                [
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                dbc.CardImg(src=pokemon.img,
+                                            id=f'{pokemon.name}',
+                                            className='class_img_pokemon'),
+                                html.P(f'{pokemon.id} - {pokemon.name.title()}',
+                                       id=f'id_nome_pokemon_{pokemon.id}',
+                                       className='class_nome_pokemon'),
+                                html.P(f' - '.join(pokemon.tipos).title(),
+                                       className='class_nome_pokemon'),
+                                dbc.Tabs(
+                                    [
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        dbc.Col(
+                                                            html.Div(
+                                                                f'{chave.upper()} - {valor}',
+                                                                id=f'id_stats_name_{chave}',
+                                                                className='class_nome_pokemon'
+                                                            ),
+                                                            md=8,
+                                                            className='teste',
+                                                            id=f'{chave}',
+
                                                         ),
-                                                        md=8,
-                                                        className='teste',
-                                                        id=f'{chave}',
+                                                    ]
+                                                ) for chave, valor in pokemon.estatisticas.items()
+                                            ],
+                                            label='Stats',
+                                            label_style={
+                                                'color': 'Black',
+                                                'margin-left': '5px',
+                                            },
+                                            id='id_label_habilidade'
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        habilidade.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px',
 
-                                                    ),
-                                                ]
-                                            ) for chave, valor in pokemon.estatisticas.items()
-                                        ],
-                                        label='Stats',
-                                        label_style={
-                                            'color': 'Black',
-                                            'margin-left': '5px',
-                                        },
-                                        id='id_label_habilidade'
-                                    ),
-                                    dbc.Tab(
-                                        [
-                                            dbc.Row(
-                                                [
-                                                    habilidade.capitalize()
-                                                ],
-                                                style={
-                                                    'margin-left': '5px',
+                                                    },
+                                                    id=f'id_habilidade{habilidade}'
+                                                ) for habilidade in pokemon.habilidade
+                                            ],
+                                            label='Habilidades',
+                                            label_style={
+                                                'color': 'Black',
+                                                'heigth': '30px'
 
-                                                },
-                                                id=f'id_habilidade{habilidade}'
-                                            ) for habilidade in pokemon.habilidade
-                                        ],
-                                        label='Habilidades',
-                                        label_style={
-                                            'color': 'Black',
-                                            'heigth': '30px'
-
-                                        },
-                                        id='id_label_habilidade',
-                                        style={'heigth': '100px'},
-                                        className='class_nome_pokemon'
-                                    ),
-                                    dbc.Tab(
-                                        [
-                                            dbc.Row(
-                                                [
-                                                    move.capitalize()
-                                                ],
-                                                style={
-                                                    'margin-left': '5px'
-                                                },
-                                                className='class_nome_pokemon',
-                                                id=f'id_moves_{move}'
-                                            ) for move in pokemon.moves
-                                        ],
-                                        label='Moves',
-                                        label_style={
-                                            'color': 'black',
-                                        },
-                                        id='id_label_moves',
-                                        className='class_habilidade',
-                                    ),
-                                    dbc.Tab(
-                                        [
-                                            dbc.Row(
-                                                [
-                                                    loc.capitalize()
-                                                ],
-                                                style={
-                                                    'margin-left': '5px'
-                                                },
-                                                className='class_nome_pokemon',
-                                                id=f'id_moves_{loc}'
-                                            ) for loc in pokemon.locations
-                                        ],
-                                        label='Location',
-                                        label_style={
-                                            'color': 'black',
-                                        },
-                                        id='id_label_moves',
-                                        className='class_habilidade',
-                                    )
-                                ]
-                            )
-                        ],
-                    ), className='class_cards_pokemons',
-                    color=f'{pokemon.cor}'
-                ) for pokemon in pokemons
-            ]
+                                            },
+                                            id='id_label_habilidade',
+                                            style={'heigth': '100px'},
+                                            className='class_nome_pokemon'
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        move.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px'
+                                                    },
+                                                    className='class_nome_pokemon',
+                                                    id=f'id_moves_{move}'
+                                                ) for move in pokemon.moves
+                                            ],
+                                            label='Moves',
+                                            label_style={
+                                                'color': 'black',
+                                            },
+                                            id='id_label_moves',
+                                            className='class_habilidade',
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        loc.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px'
+                                                    },
+                                                    className='class_nome_pokemon',
+                                                    id=f'id_moves_{loc}'
+                                                ) for loc in pokemon.locations
+                                            ],
+                                            label='Location',
+                                            label_style={
+                                                'color': 'black',
+                                            },
+                                            id='id_label_moves',
+                                            className='class_habilidade',
+                                        )
+                                    ]
+                                )
+                            ],
+                        ), className='class_cards_pokemons',
+                        color=f'{pokemon.cor}'
+                    ) for pokemon in pokemons if tipo in pokemon.tipos
+                ]
+            )
         )
-    )
+    else:
+        return dbc.Row(
+            dbc.Col(
+                [
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                dbc.CardImg(src=pokemon.img,
+                                            id=f'{pokemon.name}',
+                                            className='class_img_pokemon'),
+                                html.P(f'{pokemon.id} - {pokemon.name.title()}',
+                                       id=f'id_nome_pokemon_{pokemon.id}',
+                                       className='class_nome_pokemon'),
+                                html.P(f' - '.join(pokemon.tipos).title(),
+                                       className='class_nome_pokemon'),
+                                dbc.Tabs(
+                                    [
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        dbc.Col(
+                                                            html.Div(
+                                                                f'{chave.upper()} - {valor}',
+                                                                id=f'id_stats_name_{chave}',
+                                                                className='class_nome_pokemon'
+                                                            ),
+                                                            md=8,
+                                                            className='teste',
+                                                            id=f'{chave}',
+
+                                                        ),
+                                                    ]
+                                                ) for chave, valor in pokemon.estatisticas.items()
+                                            ],
+                                            label='Stats',
+                                            label_style={
+                                                'color': 'Black',
+                                                'margin-left': '5px',
+                                            },
+                                            id='id_label_habilidade'
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        habilidade.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px',
+
+                                                    },
+                                                    id=f'id_habilidade{habilidade}'
+                                                ) for habilidade in pokemon.habilidade
+                                            ],
+                                            label='Habilidades',
+                                            label_style={
+                                                'color': 'Black',
+                                                'heigth': '30px'
+
+                                            },
+                                            id='id_label_habilidade',
+                                            style={'heigth': '100px'},
+                                            className='class_nome_pokemon'
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        move.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px'
+                                                    },
+                                                    className='class_nome_pokemon',
+                                                    id=f'id_moves_{move}'
+                                                ) for move in pokemon.moves
+                                            ],
+                                            label='Moves',
+                                            label_style={
+                                                'color': 'black',
+                                            },
+                                            id='id_label_moves',
+                                            className='class_habilidade',
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        loc.capitalize()
+                                                    ],
+                                                    style={
+                                                        'margin-left': '5px'
+                                                    },
+                                                    className='class_nome_pokemon',
+                                                    id=f'id_moves_{loc}'
+                                                ) for loc in pokemon.locations
+                                            ],
+                                            label='Location',
+                                            label_style={
+                                                'color': 'black',
+                                            },
+                                            id='id_label_moves',
+                                            className='class_habilidade',
+                                        )
+                                    ]
+                                )
+                            ],
+                        ), className='class_cards_pokemons',
+                        color=f'{pokemon.cor}'
+                    ) for pokemon in pokemons
+                ]
+            )
+        )
 
 
 def gera_tabs(tab, tipo=None):
@@ -130,7 +245,7 @@ def gera_tabs(tab, tipo=None):
         inicio = 1
         fim = 9
         lista_pokemons = run(main(inicio, fim))
-        return gerar_cartoes(pokemons=lista_pokemons), \
+        return gerar_cartoes(pokemons=lista_pokemons, tipo=tipo), \
             [
                 {'label': f'{pokemon.id} - {pokemon.name.title()}',
                  'value': f'{pokemon.id}'}
